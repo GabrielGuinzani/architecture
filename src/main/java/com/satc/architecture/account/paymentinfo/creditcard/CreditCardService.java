1e08fe4b74ade32fb6b69a33ebd737135114916d
gabriel.186216 <gferreiraguinzani@gmail.com>
2023-10-25 21:19:45 -0300

    Aplicado Factory e componentização aninhada

---
+++ src/main/java/com/satc/architecture/account/paymentinfo/creditcard/CreditCardService.java
@@ -0,0 +1,9 @@
+package com.satc.architecture.account.paymentinfo.creditcard;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CreditCardService {
+}
